d8b2f3a1d7749ef7ce64e123e84df627789f97fc
Ankit Utekar <ankit.16952@gmail.com>
2023-06-04 21:02:48 +0530

    Making it look prettier

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import DataGrid from "react-data-grid";
 import "react-data-grid/lib/styles.css";
+
+import { Row } from "./types";
+import "./grid-styles.css";
 
 import { columnsData, rowsData } from "./data";
 
 const App: React.FC = () => {
-	return <DataGrid columns={columnsData} rows={rowsData} />;
+	const [rows, setRows] = useState(rowsData);
+
+	function rowKeyGetter(row: Row) {
+		return row.resourceName;
+	}
+
+	return (
+		<div className="rootContainer">
+			<DataGrid
+				columns={columnsData}
+				rows={rows}
+				rowKeyGetter={rowKeyGetter}
+				onRowsChange={setRows}
+				headerRowHeight={45}
+				className="rdg-light fill-grid"
+			/>
+		</div>
+	);
 };
 
 export default App;
